8b69bfe8bd36cf611b59be41a23b7d7ec3706edd
Jankie <qwq@awaish.link>
2024-01-07 11:40:21 +0800

    Add socketrocket option

--- src/web.py
+++ src/web.py
@@ -1,10 +1,33 @@
+import sys
+import json
 import flask
 import index
 import base64
 import random
 
 app = flask.Flask(__name__)
-vmess = index.Reader('./list/vmess.txt')
+
+try:
+    config = {}
+    with open('config.json','r') as f:
+        config = json.loads(f.read())
+except FileNotFoundError:
+    print('It looks like you are running for the first time, do you need vmess or socketrocket? (You can modify it later in config.json)')
+    x = input('[vmess/socketrocket]')
+    if x == 'vmess':
+        config = {'type': 'vmess'}
+    elif x == 'socketrocket':
+        config = {'type': 'socketrocket'}
+    else:
+        print('Invalid input.')
+        sys.exit(-1)
+    with open('config.json','w') as f:
+        json.dump(config, f)
+
+if config['type'] == 'vmess':
+    proxy = index.Reader('./list/vmess.txt')
+else:
+    proxy = index.Reader('./list/socketrocket.txt')
 
 @app.route('/')
 def home():
@@ -12,12 +35,12 @@ def home():
 
 @app.route('/api/v1/randomproxy')
 def random_proxy():
-    r = vmess.get_random_line()
+    r = proxy.get_random_line()
     return {'code': 200, 'result': r}
 
 @app.route('/api/v1/subscribe')
 def get_subscribe():
-    result = base64.b64encode(str(vmess).encode()).decode()
+    result = base64.b64encode(str(proxy).encode()).decode()
     return {'code': 200, 'result': result}
 
 if __name__ == '__main__':
